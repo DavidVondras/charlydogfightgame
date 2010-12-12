aeab8f4da897245aa1f78b2d939b6f4284c8f43d
charles.hetier <charles.hetier@2e9598b8-afb8-4876-edc4-84008ec66e72>
2010-12-12 14:12:44 +0000

    angle class creation mass data taken into account in the jet

---
+++ Dogfight2D/src/Angle.h
@@ -0,0 +1,81 @@
+#pragma once
+#include "Config.h"
+
+namespace df
+{
+	class Angle
+	{
+	private:
+		float _degreeValue, _radianValue;
+		bool _hasDegreeValue, _hasRadianValue;
+
+		float UpdateRadianValue(void) 
+		{
+			_radianValue = _degreeValue*PI/180.f;
+			_hasRadianValue = true;
+			return _radianValue;
+		}
+
+		float UpdateDegreeValue(void)
+		{
+			_degreeValue = _radianValue*180.f/PI;
+			_hasDegreeValue = true;
+			return _degreeValue;
+		}
+
+	public:
+		Angle(void): _degreeValue(0), _radianValue(0), _hasDegreeValue(true), _hasRadianValue(true){}
+	
+		// Get the angle in degree
+		float ToDegree(void) { return _hasDegreeValue ? _degreeValue : UpdateDegreeValue(); }
+		
+		// Get rge angle in radian
+		float ToRadian(void) { return _hasRadianValue ? _radianValue : UpdateRadianValue(); }
+
+		// Set the angle with the degree value
+		void SetDegree(const float value)
+		{
+			_degreeValue = value;
+			_hasDegreeValue = true;
+			_hasRadianValue = false;
+		}
+
+		// Set the angle with a radian value
+		void SetRadian(const float value)
+		{
+			_radianValue = value;
+			_hasRadianValue = true;
+			_hasDegreeValue = false;
+		}
+
+		// Create a point from Degree value
+		static Angle FromDegree(const float &value) 
+		{
+			Angle angle;
+			angle.SetDegree(value);
+			return angle;
+		}
+
+		// Create a point from radian value
+		static Angle FromRadian(const float &value)
+		{
+			Angle angle;
+			angle.SetRadian(value);
+			return angle;
+		}
+
+		Angle operator+ (Angle &value) 
+		{
+			return _hasDegreeValue ?
+				Angle::FromDegree(_degreeValue + value.ToDegree()) :
+				Angle::FromRadian(_radianValue + value.ToRadian());
+		}
+
+		Angle operator- (Angle &value) 
+		{
+			return _hasDegreeValue ?
+				Angle::FromDegree(_degreeValue - value.ToDegree()) :
+				Angle::FromRadian(_radianValue - value.ToRadian());
+		}
+	};
+}
--- Dogfight2D/src/Config.h
+++ Dogfight2D/src/Config.h
@@ -3,3 +3,4 @@
 #define SceneriesFolder "data\\sceneries\\"
 #define ObjectsFolder "data\\objects\\"
 #define PixelPerMeterCoef 13.f
+#define PI 3.14f
